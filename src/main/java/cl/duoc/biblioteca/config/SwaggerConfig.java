b9e9fbf6218b3e0647269bc39a044187c97344ce
felipefvega <datfiliv@gmail.com>
2026-06-12 15:53:46 -0400

    avances swagger 12-06

---
+++ src/main/java/cl/duoc/biblioteca/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package cl.duoc.biblioteca.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+
+        return new OpenAPI()
+        .info(new Info()
+                    .title("API 2026 - Biblioteca Duoc")
+                    .version("1.8")
+                .description("Documentacion de la API para el sistema de biblioteca"));
+    }
+    
+}
